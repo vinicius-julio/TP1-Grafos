189c6817ba19e782a9484414e0eee178c258f799
VinicioHaner <viniciohaner@rocketmail.com>
2021-03-28 18:29:02 -0300

    adicionada a opçao de converter json para o formato de entrada de arquivo

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from Grafo import Grafo
+from converteJSON import converteJSON
 
 nome_arquivo = input("Digite o nome do arquivo: ")
 g = Grafo.leArquivo(nome_arquivo)
 
 g.imprimeGrafo()
 g.exibeInformacoes()
+vertice = int(input())
+g.dfs(vertice)
+g.connectedComponents()
+g.NumberOfconnectedComponents()
+arqJSON = input("Digite o nome do arquivo JSON: ")
+arqOut = input("Digite o nome do arquivo de saída: ")
+converteJSON(arqJSON, arqOut)
